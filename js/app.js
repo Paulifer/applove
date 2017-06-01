56775138233897838e2a8518e9e1372bcfd76f14
paulina huañaco <pauli.hu.timm@gmail.com>
2017-06-01 02:38:14 -0400

    cree funcion modal, yfuncion dentreo de funcion modal para que se cerrar el proyecto

--- js/app.js
+++ js/app.js
@@ -3,4 +3,27 @@
  */
  /*Añadiendo modal*/
 
- 
+ (function modal(){
+    var boxs = Array.from(document.getElementsByClassName("box-services"));
+    var modal = document.getElementById("box-services-modal");
+    var modalBody, cerrar, img;
+    boxs.forEach(function(box){
+        box.addEventListener("click", function(){
+            modal.innerHTML = "";
+            modalBody = document.createElement("div");
+            modalBody.classList.add("modal-body");
+            modalBody.innerHTML = box.innerHTML;            
+            modal.appendChild(modalBody);
+            modal.classList.remove("equis");
+            cerrar = document.createElement("div");
+            cerrar.classList.add("cerrar");
+            img = document.createElement("img");
+            img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+            cerrar.appendChild(img);
+            modal.appendChild(cerrar);
+            cerrar.addEventListener("click",function(){
+                modal.classList.add("equis");
+            });
+        });     
+    });
+})();
